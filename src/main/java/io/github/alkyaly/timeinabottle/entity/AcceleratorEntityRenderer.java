0fa263077f7ca4365cbce07defeb7327ff494d26
Una Thompson <una@unascribed.com>
2021-07-26 18:42:38 -0700

    Implement a port of the original Random Things overlay

--- src/main/java/io/github/alkyaly/timeinabottle/entity/AcceleratorEntityRenderer.java
+++ src/main/java/io/github/alkyaly/timeinabottle/entity/AcceleratorEntityRenderer.java
@@ -16,7 +16,7 @@ import net.minecraft.util.math.Vector4f;
 
 public class AcceleratorEntityRenderer extends EntityRenderer<AcceleratorEntity> {
 
-    private static final Identifier BOTTLE_TEXTURE = new Identifier("timeinabottle", "textures/item/time_in_a_bottle.png");
+    private static final Identifier RING_TEXTURE = new Identifier("timeinabottle", "textures/time_ring.png");
 
     public AcceleratorEntityRenderer(EntityRendererFactory.Context ctx) {
         super(ctx);
@@ -33,11 +33,11 @@ public class AcceleratorEntityRenderer extends EntityRenderer<AcceleratorEntity>
         for (Direction dir : Direction.values()) {
             matrices.push();
             Vec3i dirVector = dir.getVector();
-            float angle = -(entity.getAngle() + (tickDelta * entity.getTimeRate())) % 360; // entity.getRemaningTime() makes the rotation jittery
+            float angle = entity.age+tickDelta;
             matrices.multiply(new Quaternion(new Vec3f(dirVector.getX(), dirVector.getY(), dirVector.getZ()), angle, true));
             MatrixStack.Entry entry = matrices.peek();
 
-            float offset = 0.5001f * (dir.getDirection() == Direction.AxisDirection.NEGATIVE ? -1 : 1); // 0.5001 is to prevent Z-fighting            
+            float offset = 0.5001f * (dir.getDirection() == Direction.AxisDirection.NEGATIVE ? -1 : 1); // 0.5001 is to prevent Z-fighting
             if (dir.getAxis() == Direction.Axis.X) {
                 vec1 = new Vector4f(offset, -0.5f, -0.5f, 1.0f);
                 vec2 = new Vector4f(offset,  0.5f, -0.5f, 1.0f);
@@ -57,13 +57,31 @@ public class AcceleratorEntityRenderer extends EntityRenderer<AcceleratorEntity>
             
             vec1.transform(entry.getModel()); vec2.transform(entry.getModel()); vec3.transform(entry.getModel()); vec4.transform(entry.getModel());
             
-            consumer.vertex(vec1.getX(), vec1.getY(), vec1.getZ()).color(1.0f, 1.0f, 1.0f, 1.0f).texture(0.0f, 1.0f)
+            int frame;
+            if (entity.getTimeRate() >= 32) {
+                frame = 5;
+            } else if (entity.getTimeRate() >= 16) {
+                frame = 4;
+            } else if (entity.getTimeRate() >= 8) {
+                frame = 3;
+            } else if (entity.getTimeRate() >= 4) {
+                frame = 2;
+            } else if (entity.getTimeRate() >= 2) {
+                frame = 1;
+            } else {
+                frame = 0;
+            }
+            
+            float minU = (frame)/6f;
+            float maxU = (frame+1)/6f;
+            
+            consumer.vertex(vec1.getX(), vec1.getY(), vec1.getZ()).color(1.0f, 1.0f, 1.0f, 1.0f).texture(minU, 1.0f)
                 .overlay(OverlayTexture.DEFAULT_UV).light(15728880).normal(dirVector.getX(), dirVector.getY(), dirVector.getZ()).next();
-            consumer.vertex(vec2.getX(), vec2.getY(), vec2.getZ()).color(1.0f, 1.0f, 1.0f, 1.0f).texture(0.0f, 0.0f)
+            consumer.vertex(vec2.getX(), vec2.getY(), vec2.getZ()).color(1.0f, 1.0f, 1.0f, 1.0f).texture(minU, 0.0f)
                 .overlay(OverlayTexture.DEFAULT_UV).light(15728880).normal(dirVector.getX(), dirVector.getY(), dirVector.getZ()).next();
-            consumer.vertex(vec3.getX(), vec3.getY(), vec3.getZ()).color(1.0f, 1.0f, 1.0f, 1.0f).texture(1.0f, 0.0f)
+            consumer.vertex(vec3.getX(), vec3.getY(), vec3.getZ()).color(1.0f, 1.0f, 1.0f, 1.0f).texture(maxU, 0.0f)
                 .overlay(OverlayTexture.DEFAULT_UV).light(15728880).normal(dirVector.getX(), dirVector.getY(), dirVector.getZ()).next();
-            consumer.vertex(vec4.getX(), vec4.getY(), vec4.getZ()).color(1.0f, 1.0f, 1.0f, 1.0f).texture(1.0f, 1.0f)
+            consumer.vertex(vec4.getX(), vec4.getY(), vec4.getZ()).color(1.0f, 1.0f, 1.0f, 1.0f).texture(maxU, 1.0f)
                 .overlay(OverlayTexture.DEFAULT_UV).light(15728880).normal(dirVector.getX(), dirVector.getY(), dirVector.getZ()).next();
             matrices.pop();
         }
@@ -72,6 +90,6 @@ public class AcceleratorEntityRenderer extends EntityRenderer<AcceleratorEntity>
 
     @Override
     public Identifier getTexture(AcceleratorEntity entity) {
-        return BOTTLE_TEXTURE;
+        return RING_TEXTURE;
     }
 }
